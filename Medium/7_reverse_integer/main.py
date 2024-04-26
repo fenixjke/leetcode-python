5d8be7e1f9a0aa974c67e32f5f4c35fb94fe7db3
Eugene Osipenko <fenixjke@gmail.com>
2024-04-26 22:40:31 +0300

    task 7

--- Medium/7_reverse_integer/main.py
+++ Medium/7_reverse_integer/main.py
@@ -4,7 +4,29 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        if Solution.is_out_of_bounds(x):
+            return 0
 
-print(321 % 10)
-print(321 // 10)
-print(divmod(321, 10))
+        is_negative = False
+        if x < 0:
+            x *= -1
+            is_negative = True
+
+        result = 0
+        while True:
+            if x < 10:
+                result = result * 10 + x
+                break
+            x, remainder = divmod(x, 10)
+            result = result * 10 + remainder
+
+        if is_negative:
+            result = -result
+
+        if Solution.is_out_of_bounds(result):
+            return 0
+        return result
+
+    @staticmethod
+    def is_out_of_bounds(x):
+        return not -2 ** 31 <= x < 2 ** 31
--- Medium/7_reverse_integer/tests/tests.py
+++ Medium/7_reverse_integer/tests/tests.py
@@ -4,15 +4,25 @@ main = importlib.import_module('Medium.7_reverse_integer.main')
 
 def test_1():
     x = 123
-    assert main.Solution().convert(x) == 321
+    assert main.Solution().reverse(x) == 321
 
 
 def test_2():
     x = -123
-    assert main.Solution().convert(x) == -321
+    assert main.Solution().reverse(x) == -321
 
 
 def test_3():
     x = 120
-    assert main.Solution().convert(x) == 21
+    assert main.Solution().reverse(x) == 21
+
+
+def test_4():
+    x = 1
+    assert main.Solution().reverse(x) == 1
+
+
+def test_5():
+    x = 1534236469
+    assert main.Solution().reverse(x) == 0
 
